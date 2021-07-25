166c0b971c365dcc279b0ca28f00ac4dd291eaf2
BasharOwainat <>
2021-07-25 11:25:42 +0000

    added loops

--- app.js
+++ app.js
@@ -8,33 +8,44 @@ alert("Do you love cars? if you do, press ok to continue")
 var favCar = prompt("Tell us what is your favorite car so we can provide you with great photos of it. You can choose from these cars you see 1)Tesla 2) Mercedes 3)Lamborghini")
 
 
-
+while(favCar != "Tesla" && favCar != "Mercedes" && favCar != "Lamborghini"){
+  favCar = prompt("Sorry you have to pick one of these (Tesla, Mercedes, Lamborghini)")
+}
 
 if(favCar == "Tesla")
 {
-  document.write("<div>"+
-    "<img src='https://teslaweekly.com/wp-content/uploads/2018/01/roadster.png' alt='Tesla' width='300' height='200' title='Tesla'> "+ "<h4> Tesla </h4>"+
+  var numOfCopies = prompt("How many copies do you want to see of Tesla? ")
+
+  for(var i = 0; i < numOfCopies; i++){  
+
+   document.write("<div>"+
     "<img src='https://businesstech.co.za/news/wp-content/uploads/2021/02/Tesla-Model-X.png' alt='Tesla' width='300' height='200' title='Tesla'> "+ "<h4> Tesla </h4>"+
     "</div>")
+  }
 }
 
 else if(favCar == "Lamborghini")
 {
-document.write("<div>"+
-    "<img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-lamborghini-huracan-evo-mmp-1-1600293029.jpg?crop=0.889xw:1.00xh;0.0561xw,0&resize=640:*' alt='Tesla' width='300' height='200' title='Lamborghini'> "+ "<h4> Lamborghini </h4>"+
-    "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQryP3v7Q3N2GhjLRGHMWeamuGZy-oBZWO9PBg5GlybfvGbxx-HKl9-o7nxAQp9p3-m1gg&usqp=CAU' alt='Tesla' width='300' height='200' title='Lamborghini'> "+ "<h4> Lamborghini </h4>" +
+  numOfCopies = prompt("How many copies do you want to see of Lamborghini? ")
+  
+  for(var i = 0; i < numOfCopies; i++){  
+    document.write("<div>"+
+    "<img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2021-lamborghini-huracan-evo-mmp-1-1600293029.jpg?crop=0.889xw:1.00xh;0.0561xw,0&resize=640:*' alt='Tesla' width='300' height='200' title='Lamborghini'> "+ "<h4> Lamborghini </h4>" +
 
     "</div>")
+  }
 }
 
 else if(favCar == "Mercedes")
 {
-document.write("<div>"+
-    "<img src='https://cdn.vox-cdn.com/thumbor/yZu9QZxpMh_3lIJU0ilrmlCLhz4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19580683/mercedes_vision_avtr_0478.jpg' alt='Mercedes' width='300' height='200' title='Mercedes'> "+ "<h4> Mercedes</h4>"+
+  numOfCopies = prompt("How many copies do you want to see of Mercedes? ")
+  
+  for(var i = 0; i < numOfCopies; i++){  
+   document.write("<div>"+ 
     "<img src='https://pbs.twimg.com/media/E6KVHAfXMAAUFXh.jpg' alt='Mercedes' width='300' height='200' title='Mercedes'> "+ "<h4> Mercedes</h4>" +
     "</div>")
+  }
 }
-else
-{
-  alert("Sorry! We don't have this car ")
-}
+
+
+
